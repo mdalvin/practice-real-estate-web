1bc40ab990423679f8e14b3ce70b90d742f819b5
mdalvin <mochamaddalvin@gmail.com>
2021-02-19 23:55:36 +0700

    toggle menu added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Hero from './components/Hero';
-import Navbar from './components/Navbar'
-import { SliderData } from './data/SliderData';
-import GlobalStyle from './globalStyle';
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import { SliderData } from "./data/SliderData";
+import GlobalStyle from "./globalStyle";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <GlobalStyle/>
-      <Navbar/>
-      <Hero slides={SliderData}/>
+      <GlobalStyle />
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
+      <Hero slides={SliderData} />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,11 +71,11 @@ const NavBtn = styled.div`
   }
 `;
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
   return (
     <Nav>
       <Logo to="/">MiCASA</Logo>
-      <MenuBars />
+      <MenuBars onClick={toggle} />
       <NavMenu>
         {menuData.map((item, index) => (
           <NavMenuLinks to={item.link} key={index}>
